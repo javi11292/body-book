9b6c2a327c526255589b9192dc45b3e40fca8387
Javier Robles Fernández <javi_1_12_92@hotmail.com>
2019-11-12 10:47:55 +0100

    Updates

---
+++ precache-manifest.c2b13cd26555f6079566e7b4feaf8c4c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8398c3014f978e15dc89211b9c473fff",
+    "url": "/body-book/index.html"
+  },
+  {
+    "revision": "2e5e536c15fa8f3b83e3",
+    "url": "/body-book/static/js/0.18f7c8eb.chunk.js"
+  },
+  {
+    "revision": "eab994a8524ba839465a",
+    "url": "/body-book/static/js/3.594574b5.chunk.js"
+  },
+  {
+    "revision": "f880843cc2e2d765642a",
+    "url": "/body-book/static/js/4.27834360.chunk.js"
+  },
+  {
+    "revision": "d8ab552449fafe10728f",
+    "url": "/body-book/static/js/5.d68e9923.chunk.js"
+  },
+  {
+    "revision": "900572b15a0f4eeb3b45",
+    "url": "/body-book/static/js/main.37855f1b.chunk.js"
+  },
+  {
+    "revision": "acc0fd2d4def0523f3d0",
+    "url": "/body-book/static/js/runtime-main.c6537119.js"
+  }
+]);
