fdc59e43c68876afa6697c3473f2fce154d95365
Javier Robles Fernández <javi_1_12_92@hotmail.com>
2019-11-12 14:41:08 +0100

    Add 404 redirect

--- client/src/components/Main/useLogic.js
+++ client/src/components/Main/useLogic.js
@@ -1,9 +1,19 @@
 import { useEffect } from "react"
+import { useHistory } from "react-router-dom"
 import { get } from "libraries/fetch"
 import useStore from "hooks/useStore"
 
 function useLogic() {
+  const history = useHistory()
   const [logged, setLogged] = useStore("logged")
+
+  useEffect(() => {
+    const location = sessionStorage.getItem("location")
+    if (location) {
+      history.push(location)
+      sessionStorage.removeItem("location")
+    }
+  }, [history])
 
   useEffect(() => {
     async function checkSession() {
