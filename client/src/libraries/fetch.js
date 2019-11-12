b05fd2ef181623a731c5983ac20abb7fd0317fa5
Javier Robles Fernández <javi_1_12_92@hotmail.com>
2019-11-12 10:57:17 +0100

    Update url

--- client/src/libraries/fetch.js
+++ client/src/libraries/fetch.js
@@ -1,34 +1,42 @@
 export async function post(path, body) {
-  const response = await fetch(`${process.env.REACT_APP_SERVER}${path}`, {
-    credentials: "include",
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(body),
-  })
-
-  if (!response.ok) return { error: response.statusText }
-
-  const text = await response.text()
   try {
-    return JSON.parse(text)
-  } catch {
-    return text
+    const response = await fetch(`${process.env.REACT_APP_SERVER}${path}`, {
+      credentials: "include",
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(body),
+    })
+
+    if (!response.ok) return { error: response.statusText }
+
+    const text = await response.text()
+    try {
+      return JSON.parse(text)
+    } catch {
+      return text
+    }
+  } catch (error) {
+    return { error: error.message }
   }
 }
 
 export async function get(path) {
-  const response = await fetch(`${process.env.REACT_APP_SERVER}${path}`, {
-    credentials: "include",
-  })
-
-  if (!response.ok) return { error: response.statusText }
-
-  const text = await response.text()
   try {
-    return JSON.parse(text)
-  } catch {
-    return text
+    const response = await fetch(`${process.env.REACT_APP_SERVER}${path}`, {
+      credentials: "include",
+    })
+
+    if (!response.ok) return { error: response.statusText }
+
+    const text = await response.text()
+    try {
+      return JSON.parse(text)
+    } catch {
+      return text
+    }
+  } catch (error) {
+    return { error: error.message }
   }
 }
