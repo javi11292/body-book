c184f04854c35e3a5ac5935c0f15f2cf9100724d
Javier Robles Fernández <javi_1_12_92@hotmail.com>
2019-11-18 10:47:54 +0100

    Fix maxage cookie

--- server/middleware/session.js
+++ server/middleware/session.js
@@ -2,6 +2,8 @@ const session = require('express-session')
 const { client: redisClient } = require("../src/redis")
 const RedisStore = require('connect-redis')(session)
 
+const DAYS = 24 * 60 * 60 * 1000
+
 module.exports = session({
   store: new RedisStore({ client: redisClient }),
   secret: "secret",
@@ -10,5 +12,6 @@ module.exports = session({
   cookie: {
     sameSite: process.env.NODE_ENV === "production" ? "none" : false,
     secure: process.env.NODE_ENV === "production",
+    maxAge: 7 * DAYS,
   },
 })
